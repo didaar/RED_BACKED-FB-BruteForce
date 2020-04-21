ad3a2f3505837a89c1331c07a304c8111c5805e0
ReD-BackeD <62339756+ReD-BackeD@users.noreply.github.com>
2020-04-21 12:35:47 +0700

    Update fb.py

--- fb.py
+++ fb.py
@@ -45,7 +45,7 @@ def function(email,passw,i):
 		return True
 	return False
 
-print('\n---------- Welcome To RED BACKED FB BruteForce ----------\n')
+print('\n---------- Welcome To RED_BACKED-FB-BruteForce ----------\n')
 file=open('passwords.txt','r')
 
 email=input('Enter Email/Username : ')
