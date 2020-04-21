ac40f160b0439290347d9386217ee94c060b37f4
ReD-BackeD <62339756+ReD-BackeD@users.noreply.github.com>
2020-04-21 12:44:32 +0700

    Update fb 02.py

--- fb 02.py
+++ fb 02.py
@@ -24,7 +24,7 @@ except:
 	print('\n\tPlease install mechanize.\n')
 	sys.exit()
 
-print('\n---------- Welcome To Facebook BruteForce ----------\n')
+print('\n---------- Welcome To RED_BACKED-FB-BruteForce ----------\n')
 file=open('passwords.txt','r')
 
 email=str(raw_input('Enter Email/Username : ').strip())
